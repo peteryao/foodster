648b8dbab88ef285caf93535b63381048eefb185
Peter Yao <peteryao916@gmail.com>
2014-10-08 10:47:20 -0700

    Removed old files. Restarting.

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from core.models import UserExtension
-# Register your models here.
-
-class UserExtensionAdmin(admin.ModelAdmin):
-	list_display = ['id', 'user', 'points', 'card', 'modified', 'created']
-
-admin.site.register(UserExtension, UserExtensionAdmin)
